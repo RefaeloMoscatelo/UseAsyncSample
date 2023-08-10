c05e967725ae6b7eabef0c9b52f9193fa67db994
Rafael <refaelomoscatelo@gmail.com>
2023-08-10 10:20:01 +0300

    Add project files.

---
+++ UseAsyncSample/Form1.Designer.cs
@@ -0,0 +1,94 @@
+﻿
+namespace UseAsyncSample
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.lblSum = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(620, 43);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(121, 37);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "button1";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // txtResult
+            // 
+            this.txtResult.Location = new System.Drawing.Point(72, 83);
+            this.txtResult.Multiline = true;
+            this.txtResult.Name = "txtResult";
+            this.txtResult.Size = new System.Drawing.Size(502, 211);
+            this.txtResult.TabIndex = 1;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(72, 34);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(502, 32);
+            this.progressBar1.TabIndex = 2;
+            // 
+            // lblSum
+            // 
+            this.lblSum.Location = new System.Drawing.Point(72, 354);
+            this.lblSum.Name = "lblSum";
+            this.lblSum.Size = new System.Drawing.Size(100, 23);
+            this.lblSum.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblSum);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.btnStart);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.TextBox txtResult;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label lblSum;
+    }
+}
+
---
+++ UseAsyncSample/Form1.cs
@@ -0,0 +1,111 @@
+﻿using Microsoft.Diagnostics.Instrumentation.Extensions.Base;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UseAsyncSample
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private Random rnd = new Random();
+        private AutoResetEvent ev = new AutoResetEvent(true);
+        private async void btnStart_Click(object sender, EventArgs e)
+        {
+            string[] urls = { "https://www.ynet.co.il", "https://www.walla.co.il", "https://www.facebbok.com" };
+            int sum = 0;
+            for (int i = 0; i < urls.Length; i++)
+            {
+                try
+                {
+                    int count = await DownloadFileAsync(urls[i], new Progress<int>( value=>progressBar1.Value = value));
+                        sum += count;
+                        this.Invoke(new Action(() =>
+                        {
+                            txtResult.AppendText(urls[i] + " Size : " + count + Environment.NewLine);
+                            lblSum.Text = sum.ToString();
+                        }));
+                }
+                catch (Exception ex)
+                {
+                    this.Invoke(new Action(() =>
+                    {
+                        txtResult.AppendText(urls[i] + " failed" + Environment.NewLine);
+                    }));
+                }
+            }
+        }
+        //private void btnStart_Click(object sender, EventArgs e)
+        //{
+        //    string[] urls = { "https://www.ynet.co.il", "https://www.walla.co.il", "https://www.facebbok.com" };
+        //    int index = 0;
+        //    int totalsignal = 0;
+        //    int sum = 0;
+        //while(totalsignal<=3)
+        //    {
+        //        try
+        //        {
+        //            ev.WaitOne();
+        //            DownloadFileAsync(urls[index]).ContinueWith(prevTask=>
+        //            {
+        //                int count = prevTask.Result;
+        //                 sum += count;
+        //                this.Invoke(new Action(() => 
+        //                {
+        //                    txtResult.AppendText(urls[index] + " Size : " + count + Environment.NewLine);
+        //                    lblSum.Text = sum.ToString();
+        //                }));
+        //                totalsignal++;
+        //                ev.Set();
+        //            });
+        //        }
+        //        catch (Exception ex)
+        //        {
+        //            this.Invoke(new Action(() =>
+        //            {
+        //                txtResult.AppendText(urls[index] + " failed" + Environment.NewLine);
+        //            }));
+        //        }
+        //        index++;
+        //    }
+        //}
+        //private void OnFileDownloadFinished(int result)
+        //{
+        //    int count = result;
+        //    sum += count;
+        //    txtResult.AppendText(urls[i] + " Size : " + count + Environment.NewLine);
+        //    lblSum.Text = sum.ToString();
+        //}
+
+        private Task<int> DownloadFileAsync(string url, IProgress<int> callback)
+        {
+            return Task<int>.Factory.StartNew(() =>
+            {
+                for (int i = 0; i <= 100; i++)
+                {
+                    Thread.Sleep(50);
+                    callback.Report(i);
+                }
+                return DownloadFile(url);
+            });
+        }
+        private int DownloadFile(string url)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Thread.Sleep(50);
+            }
+            return rnd.Next(10, 50);
+        }
+    }
+}
